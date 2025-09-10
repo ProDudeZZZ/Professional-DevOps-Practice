21f540af8432f7d07638c66aebaf0c7f4c6ab935
Sk-Sakirul <sksakirul484@gmail.com>
2025-09-10 08:49:17 +0530

    Add Docker support for the application

--- app.js
+++
@@ -1,8 +0,0 @@
-const app = require('./index');
-require("dotenv").config();
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, (req, res) => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
-})
